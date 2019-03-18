acf74d3ed0b25071651b52c620e4d5acbf671e9b
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-03-18 15:07:13 -0700

    fix type bug

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
   private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
-  private tokenTimer: NodeJS.Timer;
+  private tokenTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {
 
